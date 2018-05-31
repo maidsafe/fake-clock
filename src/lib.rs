d4502e3b23274aab500e3983b9b51fa02799baa8
Marcin S <scatman@bu.edu>
2018-05-31 17:25:36 -0400

    chore/fmt: format using rustfmt 0.8.2-nightly

--- src/lib.rs
+++ src/lib.rs
@@ -12,24 +12,31 @@
 //! A crate providing a virtual clock mimicking `std::time::Instant`'s interface, enabling full
 //! control over the flow of time during testing.
 
-#![doc(html_logo_url =
-           "https://raw.githubusercontent.com/maidsafe/QA/master/Images/maidsafe_logo.png",
-       html_favicon_url = "https://maidsafe.net/img/favicon.ico",
-       html_root_url = "https://docs.rs/fake_clock")]
-
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/maidsafe/QA/master/Images/maidsafe_logo.png",
+    html_favicon_url = "https://maidsafe.net/img/favicon.ico",
+    html_root_url = "https://docs.rs/fake_clock"
+)]
 // For explanation of lint checks, run `rustc -W help` or see
 // https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md
-#![forbid(bad_style, exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
-          unknown_crate_types, warnings)]
-#![deny(deprecated, improper_ctypes, missing_docs,
-        non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
-        private_no_mangle_fns, private_no_mangle_statics, stable_features, unconditional_recursion,
-        unknown_lints, unsafe_code, unused, unused_allocation, unused_attributes,
-        unused_comparisons, unused_features, unused_parens, while_true)]
-#![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
-        unused_qualifications, unused_results)]
-#![allow(box_pointers, missing_copy_implementations, missing_debug_implementations,
-         variant_size_differences)]
+#![forbid(
+    bad_style, exceeding_bitshifts, mutable_transmutes, no_mangle_const_items, unknown_crate_types,
+    warnings
+)]
+#![deny(
+    deprecated, improper_ctypes, missing_docs, non_shorthand_field_patterns, overflowing_literals,
+    plugin_as_library, private_no_mangle_fns, private_no_mangle_statics, stable_features,
+    unconditional_recursion, unknown_lints, unsafe_code, unused, unused_allocation,
+    unused_attributes, unused_comparisons, unused_features, unused_parens, while_true
+)]
+#![warn(
+    trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
+    unused_qualifications, unused_results
+)]
+#![allow(
+    box_pointers, missing_copy_implementations, missing_debug_implementations,
+    variant_size_differences
+)]
 
 use std::cell::RefCell;
 use std::cmp::Ordering;
@@ -50,12 +57,16 @@ thread_local!{
 impl FakeClock {
     /// Sets the thread-local fake time to the given value
     pub fn set_time(time: u64) {
-        LOCAL_TIME.with(|t| { *t.borrow_mut() = time; });
+        LOCAL_TIME.with(|t| {
+            *t.borrow_mut() = time;
+        });
     }
 
     /// Advances the thread-local fake time by the given amount of milliseconds
     pub fn advance_time(millis: u64) {
-        LOCAL_TIME.with(|t| { *t.borrow_mut() += millis; });
+        LOCAL_TIME.with(|t| {
+            *t.borrow_mut() += millis;
+        });
     }
 
     /// Returns the current thread-local fake time
