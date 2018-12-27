231d7a56b8a6e2aee3f2a7c6a5c6257e51dc486e
Pierre Chevalier <pierrechevalier83@gmail.com>
2018-12-27 13:47:55 +0000

    chore/ci: update compiler to 1.31.1
    * update compiler to latest stable: 1.31.1
    * install rustfmt and clippy with rustup on travis
    * drop nightly rust as stable has support for rustfmt and clippy
    * fix compilation errors and clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/maidsafe/QA/master/Images/maidsafe_logo.png",
     html_favicon_url = "https://maidsafe.net/img/favicon.ico",
-    test(attr(forbid(warnings))),
+    test(attr(forbid(warnings)))
 )]
 // For explanation of lint checks, run `rustc -W help` or see
 // https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md
@@ -34,8 +34,6 @@
     non_shorthand_field_patterns,
     overflowing_literals,
     plugin_as_library,
-    private_no_mangle_fns,
-    private_no_mangle_statics,
     stable_features,
     unconditional_recursion,
     unknown_lints,
@@ -75,7 +73,7 @@ pub struct FakeClock {
     time_created: u64,
 }
 
-thread_local!{
+thread_local! {
     static LOCAL_TIME: RefCell<u64> = RefCell::new(0);
 }
 
